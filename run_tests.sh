f0dfab974777d2bd242e08e49988cd42d05be8bf
Simon Martín <oleurud@gmail.com>
2018-03-15 11:18:41 +0100

    download redis-cell lib during tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-OPT_CREATE=yes # create the test environment
-OPT_DROP=yes   # drop the test environment
+OPT_CREATE=yes      # create the test environment
+OPT_DROP=yes        # drop the test environment
+OPT_REDIS_CELL=yes  # download redis cell
 
 cd $(dirname $0)
 BASEDIR=$(pwd)
@@ -36,15 +37,30 @@ die() {
 	exit 1
 }
 
+get_redis_cell() {
+  if test x"$OPT_REDIS_CELL" = xyes; then
+    echo "Downloading redis-cell"
+    wget https://github.com/brandur/redis-cell/releases/download/v0.2.2/redis-cell-v0.2.2-x86_64-unknown-linux-gnu.tar.gz > /dev/null 2>&1
+    tar xvzf redis-cell-v0.2.2-x86_64-unknown-linux-gnu.tar.gz > /dev/null 2>&1
+    rm redis-cell-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
+    rm libredis_cell.d
+  fi
+}
+
 trap 'cleanup_and_exit' 1 2 3 5 9 13
 
 while [ -n "$1" ]; do
+  echo "---> $1"
         if test "$1" = "--nodrop"; then
                 OPT_DROP=no
                 shift
                 continue
         elif test "$1" = "--nocreate"; then
                 OPT_CREATE=no
+                shift
+                continue
+        elif test "$1" = "--norediscell"; then
+                OPT_REDIS_CELL=no
                 shift
                 continue
         else
@@ -55,14 +71,16 @@ done
 if [ -z "$1" ]; then
         echo "Usage: $0 [<options>] <test> [<test>]" >&2
         echo "Options:" >&2
-        echo " --nocreate   do not create the test environment on start" >&2
-        echo " --nodrop     do not drop the test environment on exit" >&2
+        echo " --nocreate     do not create the test environment on start" >&2
+        echo " --nodrop       do not drop the test environment on exit" >&2
+        echo " --norediscell  do not download redis-cell" >&2
         exit 1
 fi
 
 TESTS=$@
 
 if test x"$OPT_CREATE" = xyes; then
+  get_redis_cell
   echo "Starting redis on port ${REDIS_PORT}"
   echo "port ${REDIS_PORT}" | redis-server - --loadmodule ./libredis_cell.so > ${BASEDIR}/test.log &
   PID_REDIS=$!
